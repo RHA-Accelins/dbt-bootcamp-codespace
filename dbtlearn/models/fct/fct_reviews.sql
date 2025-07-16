e15d9264e03cb2074f3ff6f77b88a3629177a966
RHA-Accelins <richard.alderson@accelins.com>
2025-07-16 11:18:18 +0000

    Fixed issues with review

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -1,22 +1,20 @@
-
-    {{
-      config(
-        materialized = 'incremental',
-        on_schema_change='fail'
-        )
-    }}
-
+{{
+  config(
+    materialized = 'incremental',
+    on_schema_change='fail'
+    )
+}}
 WITH src_reviews AS (
     SELECT
         *
     FROM
-        {{ref("src_reviews")}}
-
+    {{ ref('src_reviews') }}
 )
 SELECT
     *
-FROM src_reviews
+FROM
+    src_reviews
 WHERE review_text is not null
-{if {% if is_incremental() %}
+{% if is_incremental() %}
   and review_date > (select max(review_date) from {{ this }})
-{% endif %}}
+{% endif %}
--- dbtlearn/models/fct/fct_test.sql
+++
@@ -1,8 +0,0 @@
-WITH src_reviews AS (
-    SELECT * FROM src_reviews
-)
-
-SELECT
-listing_id
-from
-src_reviews
